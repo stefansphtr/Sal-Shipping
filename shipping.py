62aa04b7ca66bb281437de0a1bf69fa63733a801
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-26 12:23:18 +0700

    chore(function): completed handling the drone shipping cost based on given weight

--- shipping.py
+++ shipping.py
@@ -16,3 +16,21 @@ def calculate_ground_shipping_cost(weight):
         price_per_pound = 1.50
     return round((weight*price_per_pound) + flat_charge, 2)
   
+# Define the function drone_shipping_cost
+
+def drone_shipping_cost(weight):
+    """
+    Calculate the cost of drone shipping
+    based on the package's weight.
+    """
+    flat_charge = 0
+    if weight > 10:
+        price_per_pound = 14.25
+    elif weight > 6:
+        price_per_pound = 12
+    elif weight > 2:
+        price_per_pound = 9
+    else:
+        price_per_pound = 4.50
+    return round((weight*price_per_pound) + flat_charge, 2)
+
